0e703750e8abfcb5413fffbc7d7c6ca3eeb7e2f0
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 10:54:12 +0530

    First Program

---
+++ test.java
@@ -0,0 +1,18 @@
+public class test {
+    // public static void main(String[] args) {
+    // for (int i = 5; i >= 1; i--) {
+    // for (int j = 1; j <= i; j++) {
+    // System.out.printf("* ");
+    // }
+    // System.out.println();
+    // }
+    // }
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        int b[] = { 1, 4, 5, 6, 5 };
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+
+    }
+}
