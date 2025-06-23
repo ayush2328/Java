67b2857beb71dbf1eb958e9d18e09252ab4eeead
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 12:41:41 +0530

    File completed.

--- Loops/while_loop.java
+++ Loops/while_loop.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class while_loop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter the number of loop: ");
         int x = sc.nextInt();
         int i = 1;
-        while (i < x) {
-            System.out.printf("%d While Loop%n", i);
+        while (i <= x) {
+            System.out.printf("%d) While Loop%n", i);
             i++;
         }
     }
