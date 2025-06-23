9b3811c8d01ae9ac4e8c014e202e3cb7aafff56e
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 11:38:50 +0530

    Learing For loop

---
+++ Loops/for_loops.java
@@ -0,0 +1,9 @@
+public class for_loops {
+    public static void main(String[] args) {
+        int x = 5;
+        for (int i = 1; i <= x; i++) {
+            System.out.println("Heyy");
+        }
+    }
+
+}
