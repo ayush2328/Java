415e6535014311c0e833a071587331eab9db3fe3
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 12:24:08 +0530

    While Loop

---
+++ Loops/while_loop.java
@@ -0,0 +1,9 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int x = 5;
+        int i = 1;
+        while (i < x) {
+            System.out.printf("%d While Loop%n", i);
+        }
+    }
+}
