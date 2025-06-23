9f2de28db12ba23ecf67a598425a9947847f0261
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 13:28:58 +0530

    For Each Loop Using Array

---
+++ Loops/for_each.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class for_each {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = { 4, 2, 53, 58, 9 };
+        for (int i : a) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
+}
