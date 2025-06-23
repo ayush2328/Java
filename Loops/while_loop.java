180cf93f8fd4f6f20ba035bbf07eef1d51418206
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 12:37:21 +0530

    New Dynamic while

--- Loops/while_loop.java
+++ Loops/while_loop.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class while_loop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int i = 1;
         while (i < x) {
