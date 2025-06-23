e05e2308e013ce25236bd477081093ea8328823e
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-23 15:28:36 +0530

    Basic Array Programs

---
+++ Arrays/basic_arr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class basic_arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.printf("Enter the 5 elements: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.printf("Output is: ");
+        for (int j : a) {
+            System.out.printf("%d ", j);
+        }
+        System.out.println();
+    }
+}
