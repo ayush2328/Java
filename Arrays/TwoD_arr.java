e732c37e4d8aaf543426d382b3a44cae79c85077
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-26 12:02:53 +0530

    2D Arrays Basics

---
+++ Arrays/TwoD_arr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class TwoD_arr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[2][];
+
+    }
+}
