dff0e89cc27da4e7dc10e0dce7eb8c02b039e407
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-26 12:14:58 +0530

    File Completed.

---
+++ Conditions/multi_if_else.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class multi_if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        if (x < 0) {
+            System.out.println("The given number is Negative(-ve).");
+        } else if (x == 0) {
+            System.out.println("The given Number is Zero(0).");
+        } else if (x == 1) {
+            System.out.println("The Given Number is One(1).");
+        } else if (x % 2 == 0) {
+            System.out.printf("The Number is even : %d", x);
+        } else {
+            System.out.printf("The Number is odd : %d", x);
+        }
+    }
+
+}
