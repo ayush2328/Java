ac27378333b4901bc0c412b7b021d4bde071a47c
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-06-26 12:52:02 +0530

    Switch Case Completed : )

---
+++ Conditions/switch_case.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        switch (x) {
+            case 1:
+                System.out.println("Sunday.");
+                break;
+            case 2:
+                System.out.println("Monday.");
+                break;
+            case 3:
+                System.out.println("Tuesday.");
+                break;
+            case 4:
+                System.out.println("Wednesday.");
+                break;
+            case 5:
+                System.out.println("Thrusday.");
+                break;
+            case 6:
+                System.out.println("Friday.");
+                break;
+            case 7:
+                System.out.println("Saturday.");
+                break;
+            default:
+                System.out.println("Invaild Value.");
+        }
+
+    }
+}
