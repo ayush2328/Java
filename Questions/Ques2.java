40b1bb888b095c9b8f90b997f4a81e57b1e18334
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-07-09 14:27:02 +0530

    Logical Operator in loop

--- Questions/Ques2.java
+++ Questions/Ques2.java
@@ -7,11 +7,19 @@ public class Ques2 {
         Scanner sc = new Scanner(System.in);
         System.out.printf("Enter a number: ");
         int n = sc.nextInt();
-        if (n % 2 == 0) {
-            System.out.printf("%d is even number.", n);
+        // if (n % 2 == 0) {
+        // System.out.printf("%d is even number.", n);
+        // } else {
+        // System.out.printf("%d is odd.", n);
+        // }
+
+        // Logical operator in if else loop
+        if (n % 2 == 0 && n % 3 == 0) {
+            System.out.println("Divisible");
         } else {
-            System.out.printf("%d is odd.", n);
+            System.out.println("Not Divisible.");
         }
+
         sc.close();
 
     }
