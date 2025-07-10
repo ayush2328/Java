acef3982bc3a4ef8f176d9a39a252682ed549094
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-07-10 15:14:58 +0530

    Permutation Function

--- Functions/Factorial.java
+++ Functions/Factorial.java
@@ -3,6 +3,8 @@ package Functions;
 import java.util.Scanner;
 
 public class Factorial {
+    static Scanner sc = new Scanner(System.in);
+
     public static int Fact(int n) {
         int res = 1;
         while (n >= 1) {
@@ -12,14 +14,11 @@ public class Factorial {
         return res;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Enter a number: ");
-        // Basic Factorial Program.
-        // int num = sc.nextInt();
-        // System.out.printf("%d is the factorial of %d%n", Fact(num), num);
+    // Combination Program
+    public static void Combination() {
+        System.out.printf("%nCombination Program....%n");
 
-        // Combination Program
+        System.out.printf("Enter a number: ");
         int n = sc.nextInt();
         System.out.printf("Enter the number of combination: ");
         int r = sc.nextInt();
@@ -27,7 +26,7 @@ public class Factorial {
         int rfact = Fact(r);
         int nmrfact = Fact(n - r);
         int nCr = 0;
-        if (r > n) {
+        if (r > n || n < 0 || r < 0) {
             System.out.println("Invaild!, r cannnot be greater than n.");
 
         } else {
@@ -35,6 +34,36 @@ public class Factorial {
             System.out.printf("The Answer is: %d", nCr);
         }
         System.out.println();
+    }
+
+    // Permutation Function
+    public static void Permutation() {
+        System.out.printf("%nPermutaion Program....%n");
+        System.out.printf("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.printf("Enter the number of combination: ");
+        int r = sc.nextInt();
+        int nfact = Fact(n);
+        int nmrfact = Fact(n - r);
+        int nPr = 0;
+        if (r > n || n < 0 || r < 0) {
+            System.out.println("Invaild!, Syntax.");
+
+        } else {
+            nPr = nfact / nmrfact;
+            System.out.printf("The Answer is: %d", nPr);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Basic Factorial Program.
+        // int num = sc.nextInt();
+        // System.out.printf("%d is the factorial of %d%n", Fact(num), num);
+
+        Combination();
+        Permutation();
+
         sc.close();
 
     }
