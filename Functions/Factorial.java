40ae14c64e0c5a39d42af3a143dbfec2de44c3d9
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-07-10 14:51:51 +0530

    Combinattion Program.

--- Functions/Factorial.java
+++ Functions/Factorial.java
@@ -15,9 +15,26 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.printf("Enter a number: ");
-        int num = sc.nextInt();
-        System.out.printf("%d is the factorial of %d%n", Fact(num), num);
+        // Basic Factorial Program.
+        // int num = sc.nextInt();
+        // System.out.printf("%d is the factorial of %d%n", Fact(num), num);
 
+        // Combination Program
+        int n = sc.nextInt();
+        System.out.printf("Enter the number of combination: ");
+        int r = sc.nextInt();
+        int nfact = Fact(n);
+        int rfact = Fact(r);
+        int nmrfact = Fact(n - r);
+        int nCr = 0;
+        if (r > n) {
+            System.out.println("Invaild!, r cannnot be greater than n.");
+
+        } else {
+            nCr = nfact / (rfact * nmrfact);
+            System.out.printf("The Answer is: %d", nCr);
+        }
+        System.out.println();
         sc.close();
 
     }
