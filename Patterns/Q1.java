b07af97721f2bd209c66f5a0bbf04b9f50d10742
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-07-11 12:15:11 +0530

    Simple Traingle.

--- Patterns/Q1.java
+++ Patterns/Q1.java
@@ -14,8 +14,16 @@ public class Q1 {
         // }
 
         // Print Square of Star..
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n; j++) {
+        // System.out.printf("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // right angle triangle
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.printf("* ");
             }
             System.out.println();
