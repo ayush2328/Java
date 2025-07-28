54f8d742b0cfeda3630780f52e23653d875792a4
Ayush Gupta <ayushofficial.2328@gmail.com>
2025-07-28 21:33:22 +0530

    checking positive

---
+++ Basics/Ispositive.java
@@ -0,0 +1,13 @@
+public class Ispositive {
+    public static void main(String[] args) {
+        int a = 20;
+        if (a > 0) {
+            System.out.println("the number is positive");
+        } else if (a == 0) {
+            System.out.println("the number is zero");
+        } else {
+            System.out.println("the number is negative");
+        }
+    }
+
+}
